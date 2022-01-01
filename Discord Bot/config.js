aafe8324f7a6c935cd2b0b2a99f61232d6e88f2c
akenofu <karim.hm.hussein@gmail.com>
2022-01-01 08:39:38 +0200

    Base Bot Functionality

---
+++ Discord Bot/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+module.exports = {
+    app: {
+        px: '+',
+        token:  process.env.token,
+        playing: 'Cool'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'DJ',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 100,
+        loopMessage: false,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
