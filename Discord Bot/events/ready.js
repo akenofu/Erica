888829438836aec5b1ecd562a737ad33f4c18328
akenofu <karim.hm.hussein@gmail.com>
2022-01-01 11:08:07 +0200

    Added Web Requests to statistics service

--- Discord Bot/events/ready.js
+++ Discord Bot/events/ready.js
@@ -1,5 +1,25 @@
-module.exports = async (client) => {
-    console.log(`Logged to the client ${client.user.username}\n-> Ready on ${client.guilds.cache.size} servers for a total of ${client.users.cache.size} users`);
+const { put, post, Axios } = require("axios");
 
-    client.user.setActivity(client.config.app.playing);
+module.exports = async (client) => {
+  console.log(
+    `Logged to the client ${client.user.username}\n-> Ready on ${client.guilds.cache.size} servers for a total of ${client.users.cache.size} users`
+  );
+
+  const guilds = client.guilds.cache.map((guild) => {
+    return {
+      serverName: guild.name,
+      serverID: guild.id,
+    };
+  });
+
+  Promise.all(
+    guilds.map((guild) => {
+      return put(`${process.env.statisticsAPI}/api/v1/statistics/servers`, {
+        server: guild,
+      });
+    })
+  ).then(res => {})
+  .catch(err => console.log)
+
+  client.user.setActivity(client.config.app.playing);
 };
